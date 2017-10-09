c657025449268205948569fe8f28ae90105a30dc
henrique moura <h3dema@users.noreply.github.com>
2017-10-09 07:40:31 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ DOCKER=`docker ps -f name=wifi-container | wc -l`
 if (( $DOCKER == 1 ))
 then
     # run the container
-    docker run -dit --device /dev/rfkill:/dev/rfkill --net=bridge --cap-add=NET_ADMIN --cap-add=SYS_MODULE --name=wifi-container wifi-container
+    docker run -dit --device /dev/rfkill:/dev/rfkill --net=bridge --cap-add=NET_ADMIN --cap-add=SYS_MODULE wifi-container
     # redirects USB wireless phy devices to the container
-    /opt/docker-wifi/pidbind.sh &
+    sudo /opt/docker-wifi/pidbind.sh &
 fi
 
 docker exec -ti wifi-container bash
