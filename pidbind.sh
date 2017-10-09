0ee5377910b1c6ace8856d2c267b00997d191456
henrique moura <h3dema@users.noreply.github.com>
2017-10-09 08:00:00 -0300

    Update pidbind.sh

--- pidbind.sh
+++ pidbind.sh
@@ -2,10 +2,10 @@
 #If positive, moves the interface to the namespace of the container
 
 #Checks if the container has already loaded. 
-LINES=`docker ps -a | wc -l`
-while [[ $LINES == 1 ]];
+LINES=`docker ps -a | grep wifi-container | grep Up | wc -l`
+while [[ $LINES != 1 ]];
 do
-	LINES=`docker ps -a | wc -l`
+	LINES=`docker ps -a | grep wifi-container | grep Up | wc -l`
 done
 
 # network name space directory
@@ -17,18 +17,22 @@ then
 fi
 
 #Gets the PID of the running container
-PID=$(docker inspect -f '{{.State.Pid}}' wifi-container)
+# PID=$(docker inspect -f '{{.State.Pid}}' wifi-container)
+PID=`ps axf | grep wifi-container | grep -v grep | grep docker | awk '{print $1}'`
 
 #Make a link for the interface inside the process PID
 sudo ln -s /proc/$PID/ns/net /var/run/netns/$PID
 
-#Move the interface to the process' namespace
-sudo iw phy phy0 set netns $PID
+INTF_NAME="wlan0"
+INTF_PHY="phy0"
+
+# Move the interface to the process' namespace
+# NOTE: the following command only works if the interface driver is installed in the physical computer
+#       otherwise **iw** don't know about the interface
+sudo iw phy $INTF_PHY set netns $PID
 
 #Bring up the interface inside the container
-INTF_NAME="wlan0"
 sudo ip netns exec $PID ip link set $INTF_NAME up
 
 #After these steps, the interface will be shown inside the container.
-#Run 'ifconfig' and you will see wlp2s0, with no IP address set, but up and running	
-
+#Run 'ifconfig' (inside the container) and you will see INTF_NAME, with no IP address set, but up and running	
